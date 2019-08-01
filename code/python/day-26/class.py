fdd0fa437f01bdfce38a7a3d2a4b08c3843881a7
KANHAIYA JI <kanhaiyaji14@gmail.com>
2019-08-01 20:18:13 +0530

    Day 26 Problems Solved

---
+++ code/python/day-26/class.py
@@ -0,0 +1,10 @@
+class student:
+
+    #class Attributes
+    species = "human"
+
+    #initializer/instance attributes
+    def __init__(self,name,rollno,age):
+        self.name = name
+        self.rollno = rollno
+        self.age = age
