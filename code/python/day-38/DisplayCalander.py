5203ab518e4166bf98dac4587ce81af449864b43
KANHAIYA JI <kanhaiyaji14@gmail.com>
2019-08-12 15:54:18 +0530

    Day 38 Problems Solved

--- code/python/day-38/DisplayCalander.py
+++ code/python/day-38/DisplayCalander.py
@@ -1,11 +1,14 @@
-import calendar
+try:
+    import calendar
 
-yy = int(input("Enter year in yyyy format: "))
-mm = int(input("Enter month in mm format: "))
+    yy = int(input("Enter year in yyyy format: "))
+    mm = int(input("Enter month in mm format: "))
 
-print(calendar.month(yy,mm))
+    print(calendar.month(yy,mm))
 
-if calendar.isleap(yy):
-    print(yy,"is leap year.")
-    # if it is leap year then whole year calendar will be printed.
-    print(calendar.prcal(yy))
+    if calendar.isleap(yy):
+        print(yy,"is leap year.")
+        # if it is leap year then whole year calendar will be printed.
+        print(calendar.prcal(yy))
+except:
+    print("Please Enter year in the form of yyyy and month in the form of mm(integer value only).")
